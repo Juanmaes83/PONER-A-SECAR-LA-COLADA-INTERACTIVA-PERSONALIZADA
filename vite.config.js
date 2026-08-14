09096a79219c89a7d4654798619d26d7b43a1cf1
Juanmaes83 <juanmaes83@gmail.com>
2026-08-14 11:54:18 +0200

    build(v3): inject production studio entry

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  plugins: [{
+    name: 'hanging-media-v3-entry',
+    transformIndexHtml() {
+      return {
+        tags: [{
+          tag: 'script',
+          attrs: { type: 'module', src: './src/v3-studio.js' },
+          injectTo: 'body'
+        }]
+      };
+    }
+  }],
   build: {
     target: 'es2022',
     sourcemap: true
